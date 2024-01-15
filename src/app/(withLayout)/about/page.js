975f197629f44fd5edaf29dc3bd71380b248e1ed
musabtheexpensive <musabalmahi53@gmail.com>
2024-01-16 00:31:01 +0600

    grouping route implemented in specific field

---
+++ src/app/(withLayout)/about/page.js
@@ -0,0 +1,10 @@
+
+const AboutPage = () => {
+    return (
+        <div>
+          <h1>About Page with Header Footer</h1>  
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/(withLayout)/contact/page.js
@@ -0,0 +1,10 @@
+
+const ContactPage = () => {
+    return (
+        <div>
+        <h2>this is contact page with header footer</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
